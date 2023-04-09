e1cbe2567ccc12d8783c6535ea01de6947bfe87b
abhishekavenger <abhishekavenger995699@gmail.com>
2023-04-09 16:01:14 +0530

    initial commit

---
+++ ag.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// int32_t main()
+int main()
+{
+    
+
+    int a = 3120;
+    cout << " the multi" << a * 4 << endl;
+
+    return 0;
+}
